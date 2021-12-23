893b1bdaa1e2edf88386d2bb2f221fb9206d0c19
Emely Alonzo <emelyalonzom@gmail.com>
2021-12-23 22:58:04 +0100

    Iteration#2

--- upgrade-basic-algorithms/exercises.js
+++ upgrade-basic-algorithms/exercises.js
@@ -1,3 +1,4 @@
+//Iteración #1
 // 1.1 Create a variable named myFavoriteHero, which value is Hulk.
 const myFavouriteHero = "Hulk";
 // 1.2 Create a variable called x, which value is 50.
@@ -7,3 +8,28 @@ const h = 5;
 const y = 10;
 // 1.4 Create a variable called 'z' which value is 'h' + 'y'.
 const z = h + y;
+
+//Iteración #2
+// 1.1 Change the value of the property 'age' to 25.
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25;
+
+// 1.2 
+let firstName = 'Jon'; 
+let lastName = 'Snow'; 
+let age = 24; 
+console.log("Soy " + firstName + " " + lastName + ", tengo " + age + " años y me gustan los lobos.");
+
+// 1.3 
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+console.log(toy1.price + toy2.price);
+
+// 1.4 
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice = 25000;
+car1.finalPrice = car1.basePrice + globalBasePrice;
+car2.finalPrice = car2.basePrice + globalBasePrice;
