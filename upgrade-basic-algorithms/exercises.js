7ef75d0de8b1791927afe346f65fa27cac5bc9b3
Emely Alonzo <emelyalonzom@gmail.com>
2021-12-23 22:26:12 +0100

    Iteration#1

---
+++ upgrade-basic-algorithms/exercises.js
@@ -0,0 +1,9 @@
+// 1.1 Create a variable named myFavoriteHero, which value is Hulk.
+const myFavouriteHero = "Hulk";
+// 1.2 Create a variable called x, which value is 50.
+const x = 50;
+// 1.3 Create a variable called 'h' which value is 5 and other variable called 'y' with value 10. 
+const h = 5;
+const y = 10;
+// 1.4 Create a variable called 'z' which value is 'h' + 'y'.
+const z = h + y;
