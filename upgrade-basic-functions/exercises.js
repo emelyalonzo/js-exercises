2573b96df7ddb2383e297324d8c3df49291954f2
Emely Alonzo <emelyalonzom@gmail.com>
2021-12-24 16:42:34 +0100

    Iteración #1 Functions

---
+++ upgrade-basic-functions/exercises.js
@@ -0,0 +1,11 @@
+//Iteración #1
+function sum(numberOne , numberTwo) {
+    if (numberOne < numberTwo) {
+        console.log(numberTwo + " es el mayor");
+    } else if (numberOne > numberTwo) {
+        console.log(numberOne + " es el mayor");
+    } else {
+        console.log("Ninguno es mayor que el otro :S");
+    }
+}
+
