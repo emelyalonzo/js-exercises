8ac677dc60ffd7b61de7f0717bff85fb70c54a15
Emely Alonzo <emelyalonzom@gmail.com>
2021-12-25 20:41:17 +0100

    Iteración #2

--- upgrade-loops/exercises.js
+++ upgrade-loops/exercises.js
@@ -1,8 +1,41 @@
 //Iteración #1
-const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
+const products = [
+  "Camiseta de Pokemon",
+  "Pantalón coquinero",
+  "Gorra de gansta",
+  "Camiseta de Basket",
+  "Cinrurón de Orión",
+  "AC/DC Camiseta",
+];
 
 for (let i = 0; i < products.length; i++) {
-    if (products[i].includes('Camiseta')) {
-        console.log(products[i]);
-    }
+  if (products[i].includes("Camiseta")) {
+    console.log(products[i]);
+  }
 }
+
+//Iteración #2
+const alumns = [
+  { name: "Pepe Viruela", T1: false, T2: false, T3: true },
+  { name: "Lucia Aranda", T1: true, T2: false, T3: true },
+  { name: "Juan Miranda", T1: false, T2: true, T3: true },
+  { name: "Alfredo Blanco", T1: false, T2: false, T3: false },
+  { name: "Raquel Benito", T1: true, T2: true, T3: true },
+];
+
+function checkApproves(alumns) {
+  for (let i = 0; i < alumns.length; i++) {
+    if (
+      (alumns[i].T1 && alumns[i].T2) ||
+      (alumns[i].T1 && alumns[i].T3) ||
+      (alumns[i].T2 && alumns[i].T3)
+    ) {
+      alumns[i].isApproved = true;
+    } else {
+      alumns[i].isApproved = false;
+    }
+  }
+}
+
+checkApproves(alumns);
+console.log(alumns);
