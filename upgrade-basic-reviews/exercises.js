f281a97633e66b7f5c3a490d75635c3cf92009d5
Emely Alonzo <emelyalonzom@gmail.com>
2021-12-25 23:09:32 +0100

    Iteración #3

--- upgrade-basic-reviews/exercises.js
+++ upgrade-basic-reviews/exercises.js
@@ -66,13 +66,62 @@ const users = [
   },
 ];
 function averageSounds(array) {
-    let result = {total:0 , count:0};
-    for (const iterator of array) {
-        for (const key in iterator.favoritesSounds) {
-            result.total += iterator.favoritesSounds[key].volume;
-            result.count ++;
-        }
+  let result = { total: 0, count: 0 };
+  for (const iterator of array) {
+    for (const key in iterator.favoritesSounds) {
+      result.total += iterator.favoritesSounds[key].volume;
+      result.count++;
     }
-    return console.log("The average volume of the users is: " + result.total / result.count);
+  }
+  return console.log(
+    "The average volume of the users is: " + result.total / result.count
+  );
 }
 averageSounds(users);
+
+//Iteración #3
+// How many times has been marked as favorite a sound?
+const users2 = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+function favouriteSounds(array) {
+    let result = {};
+    for (const iterator of array) {
+      for (const key in iterator.favoritesSounds) {
+        if (key in result) {
+            result[key] += 1;
+        } else {
+            result[key] = 1;
+        }
+      }
+    }
+    return console.log(result);
+  }
+  favouriteSounds(users2);
