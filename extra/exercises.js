113b0a33b4f4187ef12ec674f62742deb6330fc2
Emely Alonzo <emelyalonzom@gmail.com>
2021-12-26 12:45:29 +0100

    Extra exercises #8

--- extra/exercises.js
+++ extra/exercises.js
@@ -1,10 +1,10 @@
 // Function that returns true if a number is an integer, and false if not
 function integerNumber(num) {
-    return Number.isInteger(num);
+  return Number.isInteger(num);
 }
 
 // Make a function that takes as parameters two strings 'a' and 'b' and returns another string:
-// if 'a' contains something of 'b', and the position of that value in common in 'b' is smaller 
+// if 'a' contains something of 'b', and the position of that value in common in 'b' is smaller
 // than position in 'a', then the result is 'b' before 'a'. If 'a' do not contains something of 'b',
 // then the result is 'a' before 'b'.
 // Examples:
@@ -13,84 +13,103 @@ function integerNumber(num) {
 // (“therefore I am”, “I think”) => output: “I think, therefore I am”
 // (“Oranges with honey”, “watermelon and apple with honey”) => output: “Oranges with honeywatermelon and apple with honey”
 function mixStrings(a, b) {
-    let arrayA = a.split(" ");
-    let arrayB = b.split(" ");
+  let arrayA = a.split(" ");
+  let arrayB = b.split(" ");
 
-    for (let i = 0; i < arrayB.length; i++) {
-        if (arrayA.includes(arrayB[i])) {
-            if (arrayA.indexOf(arrayB[i]) > i) {
-                return b + ", " + a;
-            }
-        }
+  for (let i = 0; i < arrayB.length; i++) {
+    if (arrayA.includes(arrayB[i])) {
+      if (arrayA.indexOf(arrayB[i]) > i) {
+        return b + ", " + a;
+      }
     }
-    return a + b;
+  }
+  return a + b;
 }
-console.log(mixStrings('cheese', 'cake'));
-console.log(mixStrings('therefore I am', 'I think'));
-console.log(mixStrings('Oranges with honey', 'watermelon and apple with honey')); 
+console.log(mixStrings("cheese", "cake"));
+console.log(mixStrings("therefore I am", "I think"));
+console.log(
+  mixStrings("Oranges with honey", "watermelon and apple with honey")
+);
 
 // Make a function that takes as parameter a number and splits it in digits.
 // (10) => output: [1,0]
 // (13.4) => output: [1,3,'.',4]
 // (-13.4) => output: ['-',1,3,'.',4]
 function separate(num) {
-    let stringNumber = num.toString();
-    let array = [];
-    for (let i = 0; i < stringNumber.length; i++) {
-        if (stringNumber[i] === ".") {
-            array.push (".");
-        } else if (stringNumber[i] === "-"){
-            array.push ("-");
-        } else {
-            array.push(Number(stringNumber[i]));
-        }
+  let stringNumber = num.toString();
+  let array = [];
+  for (let i = 0; i < stringNumber.length; i++) {
+    if (stringNumber[i] === ".") {
+      array.push(".");
+    } else if (stringNumber[i] === "-") {
+      array.push("-");
+    } else {
+      array.push(Number(stringNumber[i]));
     }
-    return array;
+  }
+  return array;
 }
 console.log(separate(10));
 console.log(separate(13.4));
 console.log(separate(-13.4));
 
-// Make a function that takes an array as parameters and checks if all elements are the 
+// Make a function that takes an array as parameters and checks if all elements are the
 // same in value and type. If positive, returns true, if not returns false
 // ([true, true, true, true]) => output: true
 // (['10',10,10,10]) => output: false
 function sameValue(array) {
-    for (let i = 0; i < array.length; i++) {
-        if (!(array[0] === array[i])) {
-            return false;
-        }
+  for (let i = 0; i < array.length; i++) {
+    if (!(array[0] === array[i])) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 console.log(sameValue([true, true, true, true]));
-console.log(sameValue(['10',10,10,10]));
+console.log(sameValue(["10", 10, 10, 10]));
 
 // Show in console an array of numbers sort descending
 function sortDescending(array) {
-    array.sort(function(a, b){return b-a});
-    return array;
+  array.sort(function (a, b) {
+    return b - a;
+  });
+  return array;
 }
-console.log(sortDescending([2,3,1,34,2,342,0.3]));
+console.log(sortDescending([2, 3, 1, 34, 2, 342, 0.3]));
 
 // Given an array show in console another array sort alphabetically
 // ['b', 'c', 'd', 'a'] => output: ['a', 'b', 'c', 'd']
-let array = ['b', 'c', 'd', 'a'];
+let array = ["b", "c", "d", "a"];
 console.log(array.sort());
 
 // Make a function that takes as parameter an array of strings and shows in console the longest string
 // (['help', 'me']) => output: “help”
 function longestString(array) {
-    let longest = 0;
-    let result = "";
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].length > longest){
-            result = array[i];
-            longest = array[i].length; 
-        }
+  let longest = 0;
+  let result = "";
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length > longest) {
+      result = array[i];
+      longest = array[i].length;
     }
-    return result;
+  }
+  return result;
 }
 
-console.log(longestString(['help', 'me']));
-console.log(longestString(['hello world', 'helloworld']));
+console.log(longestString(["help", "me"]));
+console.log(longestString(["hello world", "helloworld"]));
+
+// Crea una función que tome dos arrays de números como parámetros. Une ambos
+// arrays, remueve valores duplicados (si hay alguno) y ordena los números en orden
+// ascendente.
+function merge(array1, array2) {
+  let mixed = array1.concat(array2);
+  let withoutDuplicates = mixed.filter(
+    (item, pos) => mixed.indexOf(item) === pos
+  );
+  withoutDuplicates.sort(function (a, b) {
+    return a - b;
+  });
+  return withoutDuplicates;
+}
+console.log(merge([1, 2, 3, 0.3], [1, 4, 89, 2, 3, 0.1])); //output: [0.1, 0.3, 1, 2, 3, 4, 89]
