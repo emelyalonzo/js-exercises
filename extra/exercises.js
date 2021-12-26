d4907812492914be2b7fb28c64aeac665fd33761
Emely Alonzo <emelyalonzom@gmail.com>
2021-12-26 11:38:48 +0100

    Extra exercises #2

--- extra/exercises.js
+++ extra/exercises.js
@@ -3,20 +3,28 @@ function integerNumber(num) {
     return Number.isInteger(num);
 }
 
-// Crea una función que tome como parámetros dos strings, puedes llamarlos a y b. Si
-// a contiene b, añade b al principio de a. Si no, añade b al final. Por ejemplo:
+// Make a function that takes as parameters two strings 'a' and 'b' and returns another string:
+// if 'a' contains something of 'b', and the position of that value in common in 'b' is smaller 
+// than position in 'a', then the result is 'b' before 'a'. If 'a' do not contains something of 'b',
+// then the result is 'a' before 'b'.
+// Examples:
 // (“cheese”, “cake”) => output: “cheesecake”
 // (“Java”, “script”) => output: “JavaScript”
 // (“therefore I am”, “I think”) => output: “I think, therefore I am”
+// (“Oranges with honey”, “watermelon and apple with honey”) => output: “Oranges with honeywatermelon and apple with honey”
 function mixStrings(a, b) {
     let arrayA = a.split(" ");
     let arrayB = b.split(" ");
-    arrayB.forEach(elementB => {
-        if (a.includes(elementB)){
-            return b + a;
+
+    for (let i = 0; i < arrayB.length; i++) {
+        if (arrayA.includes(arrayB[i])) {
+            if (arrayA.indexOf(arrayB[i]) > i) {
+                return b + ", " + a;
+            }
         }
-    });
+    }
     return a + b;
 }
 console.log(mixStrings('cheese', 'cake'));
 console.log(mixStrings('therefore I am', 'I think'));
+console.log(mixStrings('Oranges with honey', 'watermelon and apple with honey')); 
